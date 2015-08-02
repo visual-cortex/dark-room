4e79b26c72a99ca885600f19456cf3f9e963c92b
Vlad Jerca <vlad.jerca@live.com>
2015-08-02 13:42:36 +0300

    N/A - Initial commit.

---
+++ DarkRoom.Core/Enums/Format.cs
@@ -0,0 +1,16 @@
+﻿namespace DarkRoom.Core.Enums
+{
+    public enum Format
+    {
+        Bmp,
+        Emf,
+        Exif,
+        Gif,
+        Icon,
+        Jpeg,
+        MemoryBmp,
+        Png,
+        Tiff,
+        Wmf
+    }
+}
---
+++ DarkRoom.Core/Models/Negative.cs
@@ -0,0 +1,36 @@
+﻿using DarkRoom.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarkRoom.Core.Models
+{
+    public class Negative
+    {
+        public Image Image { get; private set; }
+        public FileInfo ImageInfo { get; set; }
+
+        public Negative(Bitmap image) : this((Image)image) { }
+
+        public Negative(string path) : this(Image.FromFile(path)) {
+            ImageInfo = new FileInfo(path);
+        }
+
+        public Negative(Image image)
+        {
+            Image = image;
+        }
+
+        public void Save(string path, Format format = Format.Jpeg)
+        {
+            var imageFormat = typeof(ImageFormat);
+
+            Image.Save(path, (ImageFormat)imageFormat.GetProperty(format.ToString()).GetValue(format.ToString()));
+        }
+    }
+}
