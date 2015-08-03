026025390600530a468c9aa9d21769b2b401d365
vlad.jerca@gmail.com <vlad.jerca@gmail.com>
2015-08-03 17:23:07 +0300

    Filter value non static. Make it work.

--- DarkRoom.Example/Program.cs
+++ DarkRoom.Example/Program.cs
@@ -3,6 +3,7 @@ using DarkRoom.Core;
 using System;
 using DarkRoom.Core.Utils;
 using DarkRoom.Core.Enums;
+using System.Threading.Tasks;
 
 namespace DarkRoom.Example
 {
@@ -10,22 +11,46 @@ namespace DarkRoom.Example
     {
         static void Main(string[] args)
         {
-            Negative img = new Negative("sample.jpg");
+            Negative img = new Negative("sample.jpg"),
+                     image2 = new Negative("sample.jpg");
 
-            Darkroom editor = new Darkroom(img);
-            editor
+            Darkroom editor = new Darkroom(img),
+                     editor2 = new Darkroom(image2);
+
+            
+            Task.Run(() => {
+                editor
                     //.BlackAndWhite(BlackAndWhiteMode.Regular)
                     //.Invert()
                     //.Contrast(50)
                     //.Brightness(10)
-                    //.Saturation(50)
+            .Saturation(50)
                     //.Vibrance(-50)
                     //.Gammma(-50)
                     //.Noise(25)
                     //.Sepia()
                     //.Hue(45)
-                    .Wash()
-                    .Develop(string.Format(@"{0}.png", Environment.TickCount));
+            .Wash()
+            .Develop(string.Format(@"{0}.png", Environment.TickCount));
+            });
+
+            Task.Run(() => {
+                editor2
+                    //.BlackAndWhite(BlackAndWhiteMode.Regular)
+                    //.Invert()
+                    //.Contrast(50)
+                    //.Brightness(10)
+             .Saturation(-50)
+                    //.Vibrance(-50)
+                    //.Gammma(-50)
+                    //.Noise(25)
+                    //.Sepia()
+                    //.Hue(45)
+             .Wash()
+             .Develop(string.Format(@"{0}.png", Environment.TickCount));
+            });
+
+            Console.ReadLine();
         }
     }
 }
